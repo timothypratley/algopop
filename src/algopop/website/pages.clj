a5d59000fa77caf409d781b7ea757e01ebe824c8
Timothy Pratley <timothypratley@gmail.com>
2015-05-16 22:13:59 -0700

    title

--- src/algopop/website/pages.clj
+++ src/algopop/website/pages.clj
@@ -4,6 +4,7 @@
 (defn home [dev?]
   (page/html5
    [:head
+    [:title "Algopop"]
     [:meta {:charset "utf-8"}]
     [:meta {:http-equiv "X-UA-Compatible" :content "IE edge,chrome 1"}]
     [:meta {:name "viewport" :content "width device-width, initial-scale 1"}]
@@ -24,7 +25,6 @@
     [:meta {:name "msapplication-TileColor" :content "#da532c"}]
     [:meta {:name "msapplication-TileImage" :content "/mstile-144x144.png"}]
     [:meta {:name "theme-color" :content "#ffffff"}]
-
     [:link {:rel "stylesheet" :href "css/site.css"}]
     [:link {:rel "stylesheet" :href "//netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"}]
     [:script {:src "https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"}]
